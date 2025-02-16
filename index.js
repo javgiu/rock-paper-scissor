22abd1f895465a1d929637deef359ba7d6837645
javgiu <javypicallo@gmail.com>
2025-02-15 23:39:50 -0500

    Add some buttons and text. Need work in the logic

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-console.log("Hello World!")
-
-// Create function for the computer choice
-
 function getComputerChoice () {
     let choice = Math.floor ( Math.random () * 3 ) + 1;
 
@@ -15,107 +11,130 @@ function getComputerChoice () {
         case 3:
             return "scissors";
             break;
-    }
-}
+    };
+};
 
-// Create function for the player choice 
+function playGame() {
 
-function getHumanChoice () {
+    function playRound (humanChoice, computerChoice) {
 
-    let choice;
-
-    do {
-        choice = prompt("Select:\n   1 for rock.\n   2 for paper.\n   3 for scissors");
+        if (humanChoice == "rock" && computerChoice == "scissors") {
+            
+            messagePara.textContent = "You Win! Rock beats Scissors";
+            message.appendChild(messagePara);
+            humanScore++;
+            playerScoreSpan.textContent = humanScore;
+        } 
+        if (humanChoice == "paper" && computerChoice == "rock") {
+            
+            messagePara.textContent = "You Win! Paper beats Rock";
+            message.appendChild(messagePara);
+            humanScore++;
+            playerScoreSpan.textContent = humanScore;
+        } 
+        if (humanChoice == "scissors" && computerChoice == "paper") {
+            
+            messagePara.textContent = "You Win! Scissors beats Paper";
+            message.appendChild(messagePara);
+            humanScore++;
+            playerScoreSpan.textContent = humanScore;
+        } 
+        if (humanChoice == "rock" && computerChoice == "paper") {
+            
+            messagePara.textContent = "You Lose! Paper beats Rock";
+            message.appendChild(messagePara);
+            computerScore++;
+            computerScoreSpan.textContent = computerScore;
+        } 
+        if (humanChoice == "paper" && computerChoice == "scissors") {
+            
+            messagePara.textContent = "You Lose! Scissors beats Paper";
+            message.appendChild(messagePara);
+            computerScore++;
+            computerScoreSpan.textContent = computerScore;
+        } 
+        if (humanChoice == "scissors" && computerChoice == "rock") {
+            
+            messagePara.textContent = "You Lose! Rock beats Scissors";
+            message.appendChild(messagePara);
+            computerScore++;
+            computerScoreSpan.textContent = computerScore;
+        } 
+        if (humanChoice == "rock" && computerChoice == "rock") {
+            
+            messagePara.textContent = "Draw! Rock and Rock";
+            message.appendChild(messagePara);
+        } 
     
-
-        switch (choice) {
-            case "1":
-                return "rock";
-                break;
-            case "2":
-                return "paper";
-                break;
-            case "3":
-                return "scissors";
-                break;
-            default:
-                alert("Wrong choice!");
-                choice = false;
-                break;
-        }
-    }
-    while (!choice);
-}
-
-// Create general function where the game takes place
-
-function playGame () {
-
-    // Create variables to store human and computer scores
+        if (humanChoice == "paper" && computerChoice == "paper") {
+            
+            messagePara.textContent = "Draw! Paper and Paper";
+            message.appendChild(messagePara);
+        } 
+    
+        if (humanChoice == "scissors" && computerChoice == "scissors") {
+            
+            messagePara.textContent = "Draw! Scissors and Scissors";
+            message.appendChild(messagePara);
+        } 
+    
+    
+    };
 
     let humanScore = 0;
     let computerScore = 0;
+    let humanChoice;
+    let computerChoice;
 
-    // Function with the logic of the game on each round, it decides who win and who loses and modify the score
 
-    function playRound (humanChoice, computerChoice) {
-        console.log(`You: ${humanChoice.toUpperCase()}`);
-        console.log(`Computer: ${computerChoice.toUpperCase()}`);
     
-        if (humanChoice == "rock" && computerChoice == "scissors") {
-            console.log("You Win! Rock beats Scissors");
-            humanScore++;
-        } 
-        if (humanChoice == "paper" && computerChoice == "rock") {
-            console.log("You Win! Paper beats Rock");
-            humanScore++;
-        } 
-        if (humanChoice == "scissors" && computerChoice == "paper") {
-            console.log("You Win! Scissors beats Paper");
-            humanScore++;
-        } 
-        if (humanChoice == "rock" && computerChoice == "paper") {
-            console.log("You Lose! Paper beats Rock");
-            computerScore++;
-        } 
-        if (humanChoice == "paper" && computerChoice == "scissors") {
-            console.log("You Lose! Scissors beats Paper");
-            computerScore++;
-        } 
-        if (humanChoice == "scissors" && computerChoice == "rock") {
-            console.log("You Lose! Rock beats Scissors");
-            computerScore++;
-        } 
-        if (humanChoice == "rock" && computerChoice == "rock") {
-            console.log("Draw! Rock and Rock");
-        } 
-        if (humanChoice == "paper" && computerChoice == "paper") {
-            console.log("Draw! Paper and Paper");
-        } 
-        if (humanChoice == "scissors" && computerChoice == "scissors") {
-            console.log("Draw! Scissors and Scissors");
-        } 
-    
-    }
 
-    // Repeat until the player or the computer reach 3 points
+    buttons.forEach( (button) => {
 
-    while ( humanScore < 3 && computerScore < 3 ) {
+            
+        button.addEventListener("click", (e) => {
+                
+        
+            humanChoice = e.target.textContent.toLowerCase();
+            computerChoice = getComputerChoice();
+        
+            playRound(humanChoice, computerChoice);
 
-        playRound (getHumanChoice(), getComputerChoice());
+            if ( humanScore == 5 ) {
+                resultsPara.textContent = `You Win The Game!\nYou: ${humanScore}\nComputer: ${computerScore}`;
+                results.appendChild(resultsPara);
+                
+            } else if ( computerScore == 5 ) {
+                resultsPara.textContent = `You Lose The Game!\nYou: ${humanScore}\nComputer: ${computerScore}`;
+                results.appendChild(resultsPara);
 
-    }
+            }
 
-    //Declare the game result
+            
+        });
+            
+    });
+  
+};
 
-    if ( humanScore == 3 ) {
-        console.log(`You Win The Game!\nYou: ${humanScore}\nComputer: ${computerScore}`);
-    } else if ( computerScore == 3 ) {
-        console.log(`You Lose The Game!\nYou: ${humanScore}\nComputer: ${computerScore}`);
-    }
+const buttons = document.querySelectorAll("button");
 
-}
+const playerScoreSpan = document.querySelector("#playerScore");
+const computerScoreSpan = document.querySelector("#computerScore");
 
-// Begin the game
+const message = document.querySelector(".message");
+const messagePara = document.createElement("p");
+
+const results = document.querySelector(".results");
+const resultsPara = document.createElement("p");
 
 playGame();
+
+
+
+
+
+
+
+
+
